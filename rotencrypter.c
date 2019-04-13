99b5d4df96807ac5d7437b669be0fa23c2f84024
C3324583 <48045839+C3324583@users.noreply.github.com>
2019-04-13 14:57:26 +1000

    Update rotencrypter.c

--- rotencrypter.c
+++ rotencrypter.c
@@ -4,13 +4,13 @@
 
 int main(){
     //initialising variables
-	char text[126], element;
+	char text[1000], element;
 	int n, i, rotationkey;
 	
 	printf("Enter message: ");//prompt the user to imput a message
-	scanf(" %[^\n]s", text);//store the text imputted by the user in the array 'text'
+	scanf(" %[^\n]s", text);//store the text imputted by the user in the array "text"
 	printf("Enter rotation key: ");//prompt the user to imput a rotation key
-	scanf("%d", &rotationkey);//store the rotation value in the variable 'rotation'
+	scanf("%d", &rotationkey);//store the rotation value in the variable "rotation"
 	
 	//accounting for rotation keys that are not between 0 and 25 and translating them into their corresponding key within the range
 	if(rotationkey>25){
@@ -23,25 +23,25 @@ int main(){
 	    rotationkey = 26 - rotationkey;
 	}
 	
-	//this for loop executes the rotation for all letters in the array 'text' until the element occupying index i is empty
+	//this for loop executes the rotation for all letters in the array "text" until the element occupying index i is empty
 	for(i = 0; text[i] != '\0'; ++i){
 		element = text[i]; //assigning the value at index "i" of "text" to the variable element
 		
-		//Each lowercase letter of the string is rotated by the "rotationkey"
+		//Each lowercase letter of the string is turned into UPPERCASE by subtracting 32 (according to the ASCII standard) and then rotated by the "rotationkey"
 		if(element >= 'a' && element <= 'z'){
-			element = element + rotationkey;
+			element = element + rotationkey - 32;
 			//If the rotation causes the element to have a value outside 'a'....'z' (according to the ASCII standard), 26 is subtracted to give the rotated letter
-			if(element > 'z' || element < 'a'){
+			if(element > 'Z' || element < 'A'){
 				element = element - 26;
 			}
 			text[i] = element; //storing the rotated letter back in the array "text"
 		}
 		
-		//Each UPPERCASE letter is turned into lowercase by by adding 32 (according to the ASCII standard) and then rotated by the "rotationkey"
+		//Each UPPERCASE letter is rotated by the "rotationkey"
 		else if(element >= 'A' && element <= 'Z'){
-			element = element + rotationkey + 32;
+			element = element + rotationkey;
 			//If the rotation causes the element to have a value outside 'a'....'z' (according to the ASCII standard), 26 is subtracted to give the rotated letter
-			if(element > 'z' || element < 'a'){
+			if(element > 'Z' || element < 'A'){
 				element = element - 26;
 			}
 			text[i] = element; //storing the rotated letter back in the array "text"
